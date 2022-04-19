333c30e3d6304ee23dcc11508e4daaecfef25791
luiz gustavo <gustavo.limafgz@gmail.com>
2022-04-19 15:40:02 -0400

    add. full

--- 001.js
+++ 001.js
@@ -1,16 +1,27 @@
-function leiamais(){
-    var pontos=document.getElementById("pontos");
-    var maistexto=document.getElementById("mais");
-    var btnleiamais=document.getElementById("btn-02");
+function leiamais() {
+  var pontos = document.getElementById("pontos");
+  var maistexto = document.getElementById("mais");
+  var btnleiamais = document.getElementById("btn-02");
 
-    if(pontos.style.display === "none"){
-    pontos.style.display="inline";
-    maistexto.style.display="none";
-    btnleiamais.innerHTML="Leia Mais";
-    }else{
-        pontos.style.display="none";
-        maistexto.style.display="inline";
-        btnleiamais.innerHTML="Leia Menos";
-    }
+  if (pontos.style.display === "none") {
+    pontos.style.display = "inline";
+    maistexto.style.display = "none";
+    btnleiamais.innerHTML = "Leia Mais";
+  } else {
+    pontos.style.display = "none";
+    maistexto.style.display = "inline";
+    btnleiamais.innerHTML = "Leia Menos";
+  }
 }
 
+function criarTopico() {
+  var topico = document.getElementById("criar-topico");
+  var feedback = document.getElementById("feedback");
+
+  if (topico.style.display === "none") {
+    topico.style.display = "inline";
+  } else {
+    feedback.style.display = "inline";
+    topico.style.display = "none";
+  }
+}
